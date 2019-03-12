ee5ffa714d62e12491de771de2ef4d0c7a2d1358
rogue <1104837637@qq.com>
2019-03-12 17:13:45 +0800

    为userid和orderid添加唯一索引，防止用户重复秒杀

--- db/miaosha_order.sql
+++ db/miaosha_order.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50528
  File Encoding         : 65001
 
- Date: 06/03/2019 17:17:11
+ Date: 12/03/2019 17:12:39
 */
 
 SET NAMES utf8mb4;
@@ -26,7 +26,8 @@ CREATE TABLE `miaosha_order`  (
   `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
   `order_id` bigint(20) DEFAULT NULL COMMENT '订单id',
   `goods_id` bigint(20) DEFAULT NULL COMMENT '商品id',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `u_uid_gid`(`user_id`, `goods_id`) USING BTREE COMMENT '唯一索引，防止用户重复秒杀'
+) ENGINE = InnoDB AUTO_INCREMENT = 2032 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
